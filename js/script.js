916d0d55fe7aa5a724141a0bd8c32017ef23bff8
Unknown <thomas.brunner@live.de>
2018-02-14 21:39:18 +0100

    Optimized Function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-function toggleHeight() {
-  var bodyHeight = document.querySelector('.body').clientHeight;
+function removeHeight(element) {
+  element.css({'height': ''});
+  alert('hi');
+}
 
-  if ($('.element').height() == 0) {
-    $('.element').css({'height': bodyHeight});
+function toggleHeight() {
+  var parentElement = $(this).parent();
+  var toggleBody = parentElement.find('.toggle-body');
+  var contentBody = parentElement.find('.body');
+  var bodyHeight = contentBody.outerHeight();
+
+  if (toggleBody.height() == 0) {
+    toggleBody.css({'height': bodyHeight});
   } else {
-    $('.element').css({'height': ''});
+    toggleBody.css({'height': ''});
   }
 
+  toggleBody.on("click", $(this), removeHeight );
 }
+
+$('.js--toggleHeight').on("click", toggleHeight );
+
+/* function toggleHeight() {
+  var parentElement = $(this).parent();
+  var toggleBody = parentElement.find('.toggle-body');
+
+  toggleBody.slideToggle();
+
+  toggleBody.on("click", function() {
+    $(this).slideUp()
+  });
+}
+
+$('.js--toggleHeight').on("click", toggleHeight ); */
